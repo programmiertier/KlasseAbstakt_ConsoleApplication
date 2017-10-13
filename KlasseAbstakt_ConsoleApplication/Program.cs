fa1363f341b8c97bef457d6e97e1c14587ba62d1
programmiertier <zimmermannveronique@protonmail.com>
2017-10-13 09:26:31 +0200

    public
    Aufrufe der Klasse

--- KlasseAbstakt_ConsoleApplication/Program.cs
+++ KlasseAbstakt_ConsoleApplication/Program.cs
@@ -1,16 +1,49 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using static System.Console;
 
 namespace KlasseAbstakt_ConsoleApplication
 {
     class Program
     {
+        class Klasse // name immer groß
+        {
+            // Attribute oder Felder
+            // ohne einen explizieten 'Zugriffsmodifizierer'
+            // private und nicht ohne Methode erreichbar
+            int ganzZahl;
+            string zeichenKette;
+
+
+            // Methoden
+            public void zeigeAlles()
+            {
+                WriteLine("Das Objekt hat die Attribute: ");
+                WriteLine("{0}\t{1}", ganzZahl, zeichenKette);
+            }
+            public void setze_ganzZahl_auf(int wert)
+            {
+                ganzZahl = wert;
+            }
+            public void setze_zeichenKette_auf(string wert)
+            {
+                zeichenKette = wert;
+            }
+        }
+
+
+
         static void Main(string[] args)
         {
+            // eine Klasse aber keine Instanz
+            // OBjekt erzeugen oder Instanzieren
+            // Klasse nennen und Objektname festlegen
+
+            Klasse objekt = new Klasse();
+            // wäre nur möglich, wenn public objekt.ganzZahl = 22; // unerwünscht
+            objekt.setze_ganzZahl_auf(22);
+            objekt.setze_zeichenKette_auf("das ist der string");
+            objekt.zeigeAlles();
+            ReadLine();
         }
     }
 }
